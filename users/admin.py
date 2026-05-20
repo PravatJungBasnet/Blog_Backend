2892a7204de3f0142fefb90f441f09cc4c252174
Pravat Jung Basnet <pravatjungbasnet@gmail.com>
2026-05-20 22:32:47 +0545

    profile

--- users/admin.py
+++ users/admin.py
@@ -6,7 +6,6 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-
     model = User
     list_display = ["email", "first_name", "last_name", "is_staff"]
     list_filter = ["is_staff"]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
 from .models import User
@@ -14,15 +13,15 @@ class UserViewSet(ModelViewSet):
     def get_serializer_class(self):
         serializer_mapping = {"create": UserRegisterSerializer, "list": UserSerializer}
         return serializer_mapping.get(self.action, UserSerializer)
-    
-    @action(detail=False,methods=["get","put","patch"])
-    def profile(self,request):
-        user=request.user
+
+    @action(detail=False, methods=["get", "put", "patch"])
+    def profile(self, request):
+        user = request.user
         print(user.email)
         if request.method == "GET":
-            serializer=self.get_serializer(user)
-            return Response(serializer.data,status=status.HTTP_200_OK)
-        serializer=self.get_serializer(user,data=request.data,partial=True)
+            serializer = self.get_serializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = self.get_serializer(user, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_201_CREATED)
