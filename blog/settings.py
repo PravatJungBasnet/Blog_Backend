060dd81e36c797f87bd85363bd415aef7b362845
Pravat Jung Basnet <pravatjungbasnet@gmail.com>
2026-05-20 22:31:29 +0545

    profile

--- blog/settings.py
+++ blog/settings.py
@@ -76,7 +76,9 @@ TEMPLATES = [
 WSGI_APPLICATION = "blog.wsgi.application"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+
     )
 }
 
