b515d5b078d89f69e0ff3a54efa08246f2bc42c5
Pravat Jung Basnet <pravatjungbasnet@gmail.com>
2026-05-29 15:19:31 +0545

    feat:Implemented Like

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -1,9 +1,12 @@
-from .models import Blog
+from .models import Blog, Like, Comment, Bookmark
 from core.serializers import BaseSerializer
 from users.serializers import UserBriefSerializer
+from rest_framework import serializers
 
 
 class BlogSerializer(BaseSerializer):
+    like_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Blog
         fields = [
@@ -13,6 +16,7 @@ class BlogSerializer(BaseSerializer):
             "cover_image",
             "content",
             "status",
+            "like_count",
             "created_by",
             "updated_by",
             "created_at",
@@ -29,3 +33,27 @@ class BlogSerializer(BaseSerializer):
             "created_by": UserBriefSerializer,
             "updated_by": UserBriefSerializer,
         }
+
+    def get_like_count(self, obj):
+        return obj.like_set.count()
+
+
+class LikeSerializer(BaseSerializer):
+    class Meta:
+        model = Like
+        fields = ["id", "blog", "user", "is_liked", "created_at", "updated_at"]
+        read_only_fields = ["id", "user", "created_at", "updated_at"]
+
+
+class CommentSerializer(BaseSerializer):
+    class Meta:
+        model = Comment
+        fields = ["id", "blog", "user", "content", "created_at", "updated_at"]
+        read_only_fields = ["id", "user", "created_at", "updated_at"]
+
+
+class BookMarkSerializer(BaseSerializer):
+    class Meta:
+        model = Bookmark
+        fields = ["id", "blog", "user", "is_bookmarked", "created_at", "updated_at"]
+        read_only_fields = ["id", "user", "created_at", "updated_at"]
